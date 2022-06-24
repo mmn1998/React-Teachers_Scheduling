abc727246e6e32dc0821d310fa9f308e1f625e7f
mehdi <nmanymhmdmhdy@gmail.com>
2022-06-24 20:36:51 +0430

    with hosein first stage completed :)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes , Link} from 'react-router-dom'
 import FilterScreen from './screen/FilterScreen';
 import HomeScreen from './screen/HomeScreen';
 import './App.css'
@@ -7,10 +7,57 @@ import IndexScreen from './screen/IndexScreen';
 // import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {  
+    let navigationLinks = document.getElementById('navigationLinks');
+    let homeNavigationLink = document.getElementById('homeNavigationLink');
+    let filterNavigationLink = document.getElementById('filterNavifgationLink');
+    let indexNavigationLink = document.getElementById('indexNavigationLink');
+
+    const homeNavigationHandler = () => {  
+        
+        homeNavigationLink.childNodes[0].style.display='none';
+        filterNavigationLink.childNodes[0].style.display='block';
+        indexNavigationLink.childNodes[0].style.display='block';
+        navigationLinks.classList.remove('flex-container');
+        navigationLinks.classList.add('navigation-links-clicked');
+    }
+    const filterNavigationHandler = () => {        
+        navigationLinks.classList.remove('flex-container');
+        navigationLinks.classList.add('navigation-links-clicked');
+        homeNavigationLink.childNodes[0].style.display='block';
+        filterNavigationLink.childNodes[0].style.display='none';     
+        indexNavigationLink.childNodes[0].style.display='block';
+    }
+    const indexNavigationHandler = () => {        
+        homeNavigationLink.childNodes[0].style.display='block';
+        filterNavigationLink.childNodes[0].style.display='block';
+        indexNavigationLink.childNodes[0].style.display='block';
+        navigationLinks.classList.add('flex-container');
+        navigationLinks.classList.remove('navigation-links-clicked');   
+    }
   return (
 
     <Router>            
-      <IndexScreen />
+      {/* <IndexScreen /> */}
+      {/* <div style={{ height:'10rem' }}>
+        <Header searchPhrase={searchPhrase} setSearchPhrase={setSearchPhrase} />
+      </div> */}
+      <div id='navigationLinks' className='flex-container btn-c'>
+        <div id='homeNavigationLink'>       
+            <Link className='m-b-2 mbg-b' to="/home" onClick={homeNavigationHandler}>
+                مشاهده برنامه همه اساتید
+            </Link>
+        </div> 
+        <div id='filterNavifgationLink'>
+            <Link className='m-b-2 mbg-g' to="/filter" onClick={filterNavigationHandler}>
+                فیلتر کردن
+            </Link>
+        </div>
+        <div id='indexNavigationLink'>
+            <Link className='m-b-2 mbg-y' to="/index" onClick={indexNavigationHandler}>
+                صفحه اصلی
+            </Link> 
+        </div> 
+      </div>
       <Routes>
         <Route path='/index' element={<IndexScreen />}></Route>
         <Route path='/home' element={<HomeScreen />}></Route>
--- src/screen/IndexScreen.js
+++ src/screen/IndexScreen.js
@@ -1,43 +1,51 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 export default function IndexScreen() {
-    let navigationLinks = document.getElementById('navigationLinks');
-    let homeNavigationLink = document.getElementById('homeNavigationLink');
-    let filterNavigationLink = document.getElementById('homeNavigationLink');
-    let indexNavigationLink = document.getElementById('homeNavigationLink');
-    const homeNavigationHandler = () => {        
-        homeNavigationLink.style.display='none';
-        filterNavigationLink.style.display='block';
-        indexNavigationLink.style.display='block';
-        navigationLinks.classList.remove('flex-container');
-        navigationLinks.classList.add('navigation-links-clicked');
-    }
-    const filterNavigationHandler = () => {        
-        homeNavigationLink.style.display='block';
-        filterNavigationLink.style.display='none';
-        indexNavigationLink.style.display='block';
-        navigationLinks.classList.remove('flex-container');
-        navigationLinks.classList.add('navigation-links-clicked');
-    }
-    const indexNavigationHandler = () => {        
-        homeNavigationLink.style.display='block';
-        filterNavigationLink.style.display='block';
-        indexNavigationLink.style.display='none';
-        navigationLinks.classList.add('flex-container');
-        navigationLinks.classList.remove('navigation-links-clicked');   
-    }
+    // let navigationLinks = document.getElementById('navigationLinks');
+    // let homeNavigationLink = document.getElementById('homeNavigationLink');
+    // let filterNavigationLink = document.getElementById('filterNavifgationLink');
+    // let indexNavigationLink = document.getElementById('indexNavigationLink');
+
+    // const homeNavigationHandler = () => {        
+    //     homeNavigationLink.childNodes[0].style.display='none';
+    //     filterNavigationLink.childNodes[0].style.display='block';
+    //     indexNavigationLink.childNodes[0].style.display='block';
+    //     navigationLinks.classList.remove('flex-container');
+    //     navigationLinks.classList.add('navigation-links-clicked');
+    // }
+    // const filterNavigationHandler = () => {        
+    //     navigationLinks.classList.remove('flex-container');
+    //     navigationLinks.classList.add('navigation-links-clicked');
+    //     homeNavigationLink.childNodes[0].style.display='block';
+    //     filterNavigationLink.childNodes[0].style.display='none';     
+    //     indexNavigationLink.childNodes[0].style.display='block';
+    // }
+    // const indexNavigationHandler = () => {        
+    //     homeNavigationLink.childNodes[0].style.display='block';
+    //     filterNavigationLink.childNodes[0].style.display='block';
+    //     indexNavigationLink.childNodes[0].style.display='block';
+    //     navigationLinks.classList.add('flex-container');
+    //     navigationLinks.classList.remove('navigation-links-clicked');   
+    // }
   return (
-    <div id='navigationLinks' className='flex-container btn-c'>        
-        <Link className='m-b-2' to="/home" onClick={homeNavigationHandler} id='homeNavigationLink'>
-            مشاهده برنامه همه اساتید
-        </Link>
-        <Link className='m-b-2' to="/filter" onClick={filterNavigationHandler} id='filterNavifgationLink'>
-            فیلتر کردن
-        </Link>
-        <Link className='m-b-2' to="/index" onClick={indexNavigationHandler} id='indexNavigationLink'>
-        صفحه اصلی
-        </Link>  
-    </div>
+    // <div id='navigationLinks' className='flex-container btn-c'>
+    //     <div id='homeNavigationLink'>       
+    //         <Link className='m-b-2 mbg-b' to="/home" onClick={homeNavigationHandler}>
+    //             مشاهده برنامه همه اساتید
+    //         </Link>
+    //     </div> 
+    //     <div id='filterNavifgationLink'>
+    //         <Link className='m-b-2 mbg-g' to="/filter" onClick={filterNavigationHandler}>
+    //             فیلتر کردن
+    //         </Link>
+    //     </div>
+    //     <div id='indexNavigationLink'>
+    //         <Link className='m-b-2 mbg-y' to="/index" onClick={indexNavigationHandler}>
+    //             صفحه اصلی
+    //         </Link> 
+    //     </div> 
+    // </div>
+    <></>
   )
 }
